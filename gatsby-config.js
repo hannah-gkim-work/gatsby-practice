8ebf849a066404fb11a31272b2f3763fa337a6ca
Hannah Gomez Kim <hannah.gomezkim@slalom.com>
2022-01-21 18:56:50 -0600

    image works

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,22 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `video`,
+        path: `${__dirname}/src/assets/videos`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -32,8 +41,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
+      },
+    },
   ],
 }
--- src/components/Trips.js
+++ src/components/Trips.js
@@ -1,19 +1,82 @@
 //rafce react affow function component with Es7 module
 import React from "react"
 import styled from "styled-components"
+import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
+import { useStaticQuery, graphql } from "gatsby"
 
 export const Trips = () => {
+  const data = useStaticQuery(graphql`
+    query TripQuery {
+      allTripsJson {
+        edges {
+          node {
+            alt
+            button
+            name
+            img {
+              childImageSharp {
+                gatsbyImageData(
+                  width: 280
+                  height: 420
+                  placeholder: DOMINANT_COLOR
+                  formats: NO_CHANGE
+                )
+              }
+            }
+          }
+        }
+      }
+    }
+
+    # query TripQuery {
+    #   allTripsJson {
+    #     edges {
+    #       node {
+    #         alt
+    #         button
+    #         name
+    #         img {
+    #           childImageSharp {
+    #             fluid {
+    #               ...GatsbyImageSharpFluid
+    #             }
+    #           }
+    #         }
+    #       }
+    #     }
+    #   }
+    # }
+  `)
+
+  function getTrips(data) {
+    const tripsArray = []
+    data.allTripsJson.edges.forEach((item, idx) => {
+      tripsArray.push(
+        <div key={idx}>
+          {/* <Img
+            src={item.node.img.childImageSharp.fluid.src}
+            fluid={item.node.img.childImageSharp.fluid}
+          /> */}
+
+          <GatsbyImage image={item.node.img.childImageSharp.gatsbyImageData} />
+        </div>
+      )
+    })
+    return tripsArray
+  }
+
   return (
     <ProductsContainer>
       <ProductsHeading>Heading</ProductsHeading>
-      <ProductWrapper>Wrapper</ProductWrapper>
+      <ProductWrapper>{getTrips(data)}</ProductWrapper>
     </ProductsContainer>
   )
 }
+
 const ProductsContainer = styled.div`
   min-height: 100vh;
   padding: 5rem calc((100vw - 1300px) / 2);
-  background: red;
+  background: pink;
   color: #fff;
 `
 const ProductsHeading = styled.div`
